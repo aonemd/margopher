8d85326954c0450b81d61399cf5a1c752f4b52e1
aaoiki <aaoiki7@gmail.com>
2015-08-29 20:14:52 +0200

    Add some unit tests

---
+++ margopher_test.go
@@ -0,0 +1,24 @@
+package margopher
+
+import "testing"
+
+func TestGetRandomWord(t *testing.T) {
+	if getRandomWord([]string{"1", "2", "3"}) == "" {
+		t.Error("getRandomWord: it should return a string element from slice.")
+	}
+}
+
+func TestIsTerminalWord(t *testing.T) {
+	if isTerminalWord("Hey.") == false {
+		t.Error("isTerminalWord: it should return true for words ending in period.")
+	}
+}
+
+func TestParseText(t *testing.T) {
+	m := NewMargopher()
+	m.ParseText("I love cats. Cats love tuna.")
+
+	if m.states == nil {
+		t.Error("ParseText: it should initialize states.")
+	}
+}
