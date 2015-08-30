fa4a7e501d04daceee2c6cf10d90bee4ea98cfd2
aasare <aasarekode@gmail.com>
2015-08-30 14:23:42 +0200

    Move parsing methods into parsers.go

--- margopher.go
+++ margopher.go
@@ -2,13 +2,8 @@ package margopher
 
 import (
 	"bytes"
-	"github.com/PuerkitoBio/goquery"
-	"io/ioutil"
-	"log"
 	"math/rand"
-	"os"
 	"regexp"
-	"strings"
 )
 
 type margopher struct {
@@ -18,55 +13,6 @@ type margopher struct {
 // Margopher constructor
 func NewMargopher() *margopher {
 	return &margopher{states: make(map[string][]string)}
-}
-
-// Read input text into states map
-func (m *margopher) ReadText(text string) {
-	words := strings.Split(text, " ")
-
-	for i := 0; i < len(words)-1; i++ {
-		if _, ok := m.states[words[i]]; ok {
-			m.states[words[i]] = append(m.states[words[i]], words[i+1])
-		} else {
-			slice := []string{}
-			slice = append(slice, words[i+1])
-			m.states[words[i]] = slice
-		}
-	}
-}
-
-// Read text from file and send it to ReadText
-func (m *margopher) ReadFile(filePath string) {
-	// Open the file
-	file, err := os.Open(filePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Read data from the file
-	text, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Call ReadText with the text
-	m.ReadText(string(text))
-}
-
-// Read text from URL and send it to ReadText
-func (m *margopher) ReadURL(URL string) {
-	// Open web page
-	doc, err := goquery.NewDocument(URL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Search for <p></p> under <article></article> tags
-	doc.Find("article").Each(func(i int, s *goquery.Selection) {
-		text := s.Find("p").Text()
-		// Call ReadText with the text
-		m.ReadText(text)
-	})
 }
 
 // Extract keys from states map
