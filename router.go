496096c7a9920cbfef9134a3af766105b7b95b23
aonemd <aonemdsaleh@gmail.com>
2015-09-07 01:23:09 +0200

    Add API

---
+++ router.go
@@ -0,0 +1,17 @@
+package margopher
+
+import "github.com/gorilla/mux"
+
+func NewRouter(routes Routes) *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+
+	return router
+}
